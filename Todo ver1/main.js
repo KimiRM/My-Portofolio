d5c0f46e049d2a47f6ea2f4f70add185d4d3e754
KimiRM <Kimia.rm1385@gmail.com>
2026-06-22 22:07:59 +0330

    Create Todo app

--- Todo ver1/main.js
+++ Todo ver1/main.js
@@ -1,3 +1,12 @@
 const AddBtn = document.getElementById('AddTodo');
 const DeleteBtn = document.getElementById('DeleteTodo');
 const EditBtn = document.getElementById('EditTodo');
+
+
+class TodoAPP{
+    constructor(){
+        this.$TaskList = [];
+        this.$EventList = [];
+    }
+    
+}
