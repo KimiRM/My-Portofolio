407af5c59aecb196745afcdc51f1b02981c37a5a
KimiRM <Kimia.rm1385@gmail.com>
2026-06-22 22:20:40 +0330

    Create Task & Event JSON structure

--- Todo ver1/main.js
+++ Todo ver1/main.js
@@ -3,10 +3,22 @@ const DeleteBtn = document.getElementById('DeleteTodo');
 const EditBtn = document.getElementById('EditTodo');
 
 
+const Task = {
+    id : Number,
+    title: String,
+    descript = String
+}
+
+const Event = {
+    id : Number,
+    title: String,
+    descript = String
+}
+
 class TodoAPP{
     constructor(){
         this.$TaskList = [];
         this.$EventList = [];
     }
-    
+
 }
