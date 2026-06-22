f2fab3a49a2f07b9416f2bb44d6669e7b4d79ea9
KimiRM <Kimia.rm1385@gmail.com>
2026-06-22 23:06:22 +0330

    Get add section new items in main.js

--- Todo ver1/main.js
+++ Todo ver1/main.js
@@ -1,11 +1,21 @@
 const AddBtn = document.getElementById('AddTodo');
 const DeleteBtn = document.getElementById('DeleteTodo');
 const EditBtn = document.getElementById('EditTodo');
+
 const TaskEventTable = document.getElementById('TaskEventTable');
 const TodoTable = document.getElementById('TodoTable');
 const TableRightMenu = document.getElementById('RightMenu');
 const TableLeftMenu = document.getElementById('LeftMenu');
 
+const AddSection = document.getElementById('AddSection');
+const AddSectionTaskDiv = document.getElementById('AddSectionTask');
+const AddSectionEventDiv = document.getElementById('AddSectionEvent');
+const AddSectionAddBtn = document.getElementById('AddSection-AddBtn');
+const AddSectionCloseBtn = document.getElementById('AddSection-CloseBtn');
+const AddSectionTitle = document.getElementById('AddSectionTask-TitleInput');
+const AddSectionDesc = document.getElementById('AddSectionTask-DescInput');
+const AddSectionTaskBtn = document.getElementById('AddSection-AddTask');
+const AddSectionEventBtn = document.getElementById('AddSection-AddEvent');
 
 
 class TodoAPP{
@@ -15,7 +25,7 @@ class TodoAPP{
         this.$LastTaskID = 0;
         this.$LastEventID = 0;
     }
-    
+
     _AssignTaskID(Task){
         Task.id = this.$LastTaskID +1;
         this.$LastTaskID++;
